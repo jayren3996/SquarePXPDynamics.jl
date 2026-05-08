c28bd66ce62a60fed6e65047a17ca2b6b1da61ea
JieRen <32599461+jayren3996@users.noreply.github.com>
2026-05-08 17:17:11 +0100

    chore: tighten scaffold imports and ignores

--- src/TriangularPEPSDynamics.jl
+++ src/TriangularPEPSDynamics.jl
@@ -7,12 +7,14 @@ include("Gates.jl")
 include("Schedules.jl")
 include("SolvableModels.jl")
 
-using .Geometry
-using .SpinOps
-using .Models
-using .Gates
-using .Schedules
-using .SolvableModels
+using .Geometry: Coord, TRIANGULAR_DIRECTIONS, triangular_distance, neighbor, star_sites,
+                 star_color, disjoint_stars
+using .SpinOps: pauli_x, pauli_y, pauli_z, identity2, projector_up, projector_down
+using .Models: pxp_star_hamiltonian, blockade_projector, cluster_star_hamiltonian,
+               diagonal_star_hamiltonian, ising_bond_hamiltonian
+using .Gates: dense_gate, projected_gate
+using .Schedules: first_order_colors, second_order_colors
+using .SolvableModels: stabilizer_expectation_exact
 
 export Coord, TRIANGULAR_DIRECTIONS, triangular_distance, neighbor, star_sites
 export star_color, disjoint_stars
