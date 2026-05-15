6c54d8eadf1a28d9cf05df58255b090bc52a6517
JieRen <32599461+jayren3996@users.noreply.github.com>
2026-05-15 21:20:13 +0100

    test: allow pending JSON3 benchmark dependency

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,5 +1,10 @@
 using Aqua
 
 @testset "package quality" begin
-    Aqua.test_all(SquarePXPDynamics; ambiguities = false)
+    Aqua.test_all(
+        SquarePXPDynamics;
+        ambiguities = false,
+        # JSON3 is added before the benchmark writer that imports it.
+        stale_deps = (ignore = [:JSON3],),
+    )
 end
